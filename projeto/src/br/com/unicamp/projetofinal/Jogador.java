187d3c43bb92dd2f9c6bc9a2bd5055b03fafc46a
tobiaszorzetto <66502140+tobiaszorzetto@users.noreply.github.com>
2021-05-22 17:23:44 -0300

    construtor da classe jogador concluido

--- projeto/src/br/com/unicamp/projetofinal/Jogador.java
+++ projeto/src/br/com/unicamp/projetofinal/Jogador.java
@@ -1,5 +1,19 @@
 package br.com.unicamp.projetofinal;
 
 public class Jogador {
-
+	private String nome;
+	private int vida;
+	private Marcador marcador;
+	
+	public Jogador(String nome) {
+		this.nome = nome;
+		this.vida = 20;
+	}
+	
+	//getters e setters
+	
+	public void set_marcador(Marcador marcador) {
+		this.marcador = marcador;
+	}
+	
 }
